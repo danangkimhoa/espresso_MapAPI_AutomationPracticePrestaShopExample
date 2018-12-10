ed714f87a00b5c25ce8b9defdf7437d721844978
Rutger Korstjens <rutger.korstjens@gmail.com>
2018-12-10 16:22:19 +0100

    adjustment to remove apikey from string res

--- app/src/main/java/com/example/prestashopwebview/AppInfoActivity.kt
+++ app/src/main/java/com/example/prestashopwebview/AppInfoActivity.kt
@@ -10,7 +10,6 @@ import kotlinx.android.synthetic.main.bottom_navigation_bar.*
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_app_info.*
 
 
 class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback {
