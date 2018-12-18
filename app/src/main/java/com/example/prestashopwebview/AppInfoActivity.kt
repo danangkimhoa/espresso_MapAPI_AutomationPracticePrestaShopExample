0ef58e3910850598e072eb5e10c4383055e03cb9
Rutger Korstjens <rutger.korstjens@gmail.com>
2018-12-18 14:57:36 +0100

    wip: added textfield
    added a textfield and a few methods to be able to send an email with the entered text.
    Todo: add button, read field, add styling / error handling

--- app/src/main/java/com/example/prestashopwebview/AppInfoActivity.kt
+++ app/src/main/java/com/example/prestashopwebview/AppInfoActivity.kt
@@ -1,5 +1,7 @@
 package com.example.prestashopwebview
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,6 +12,7 @@ import kotlinx.android.synthetic.main.bottom_navigation_bar.*
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_app_info.*
 
 
 class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -19,6 +22,8 @@ class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
+        input_layout.setError("First name is required"); // show error
+        input_layout.setError(null);
 
         hideUnusedNavigationButtons()
         setBackBehaviourToFinishActivity()
@@ -51,6 +56,22 @@ class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback {
         with(googleMap) {
             moveCamera(CameraUpdateFactory.newLatLngZoom(VALORI_LATITUDE_AND_LONGITUDE, ZOOM_LEVEL))
             addMarker(MarkerOptions().position(VALORI_LATITUDE_AND_LONGITUDE))
+        }
+    }
+
+    private fun submitEmail(view: View) {
+        val emailAdresses = arrayOf("test@test.nl")
+        composeEmail(emailAdresses, "Test email subject", "")
+    }
+
+    private fun composeEmail(addresses: Array<String>, subject: String, body: String) {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:") // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses)
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, body)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
         }
     }
 }
