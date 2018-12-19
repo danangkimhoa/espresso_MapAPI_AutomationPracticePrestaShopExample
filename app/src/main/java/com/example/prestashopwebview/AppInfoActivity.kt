c5ef893ab1fb03204b02736c11792de532a67977
Rutger Korstjens <rutger.korstjens@gmail.com>
2018-12-19 13:52:15 +0100

    moved map related code to Fragment
    logic is working again!

--- app/src/main/java/com/example/prestashopwebview/AppInfoActivity.kt
+++ app/src/main/java/com/example/prestashopwebview/AppInfoActivity.kt
@@ -20,19 +20,7 @@ import kotlinx.android.synthetic.main.activity_app_info.*
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
 
-private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-
-class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
-
-    val VALORI_LATITUDE_AND_LONGITUDE = LatLng(52.0673941, 5.0841556)
-    val ZOOM_LEVEL = 11f
-
-    /**
-     * Flag indicating whether a requested permission has been denied after returning in
-     * [.onRequestPermissionsResult].
-     */
-    private var showPermissionDeniedDialog = false
-    private lateinit var map: GoogleMap
+class AppInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,9 +29,6 @@ class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback, EasyPermissions
         hideUnusedNavigationButtons()
         setBackBehaviourToFinishActivity()
 
-        // Get the SupportMapFragment and request notification
-        // when the map is ready to be used.
-        setUpMap()
         submit_email.setOnClickListener {
             if (email_text_box.text != null) {
                 if (email_text_box.text!!.isBlank()) {
@@ -53,30 +38,6 @@ class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback, EasyPermissions
                     submitEmail(email_text_box.text.toString())
                 }
             }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setUpMap()
-    }
-
-    private fun setUpMap() {
-        val mapFragment : SupportMapFragment? =
-                supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
-
-    }
-
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-        if (showPermissionDeniedDialog) {
-            AlertDialog.Builder(this).apply {
-                setPositiveButton("Ok", null)
-                setMessage("Permission Denied")
-                create()
-            }.show()
-            showPermissionDeniedDialog = false
         }
     }
 
@@ -92,54 +53,6 @@ class AppInfoActivity : AppCompatActivity(), OnMapReadyCallback, EasyPermissions
         info_button.visibility = View.INVISIBLE
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just move the camera to Sydney and add a marker in Sydney.
-     */
-    override fun onMapReady(googleMap: GoogleMap?) {
-        map = googleMap ?: return
-
-        with(googleMap) {
-            moveCamera(CameraUpdateFactory.newLatLngZoom(VALORI_LATITUDE_AND_LONGITUDE, ZOOM_LEVEL))
-            addMarker(MarkerOptions().position(VALORI_LATITUDE_AND_LONGITUDE))
-        }
-        map.uiSettings.isScrollGesturesEnabled = false
-        map.uiSettings.isZoomGesturesEnabled = true
-        enableMyLocation()
-
-    }
-
-    @SuppressLint("MissingPermission")
-    @AfterPermissionGranted(LOCATION_PERMISSION_REQUEST_CODE)
-    private fun enableMyLocation() {
-        // Enable the location layer. Request the location permission if needed.
-        val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-
-        if (EasyPermissions.hasPermissions(this, *permissions)) {
-            map.isMyLocationEnabled = true
-        } else {
-            // if permissions are not currently granted, request permissions
-            EasyPermissions.requestPermissions(this,
-                    "permission rationale",
-                    LOCATION_PERMISSION_REQUEST_CODE, *permissions)
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<String>,
-            grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode,
-                permissions, grantResults, this)
-    }
-
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-    }
 
     private fun submitEmail(body: String) {
         val emailAdresses = arrayOf("test@test.nl")
--- app/src/main/java/com/example/prestashopwebview/ValoriMapFragment.kt
+++ app/src/main/java/com/example/prestashopwebview/ValoriMapFragment.kt
@@ -1,21 +1,114 @@
 package com.example.prestashopwebview
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
-class ValoriMapFragment: Fragment() {
+private const val LOCATION_PERMISSION_REQUEST_CODE = 1
 
+class ValoriMapFragment: Fragment(), OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
     companion object {
         fun newInstance(): ValoriMapFragment {
             return ValoriMapFragment()
         }
     }
 
+    val VALORI_LATITUDE_AND_LONGITUDE = LatLng(52.0673941, 5.0841556)
+    val ZOOM_LEVEL = 11f
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * [.onRequestPermissionsResult].
+     */
+    private var showPermissionDeniedDialog = false
+    private lateinit var map: GoogleMap
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.valori_map_fragment, container, false)
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        setUpMap()
     }
+
+    override fun onResume() {
+        super.onResume()
+        setUpMap()
+        if (showPermissionDeniedDialog) {
+            AlertDialog.Builder(context).apply {
+                setPositiveButton("Ok", null)
+                setMessage("Permission Denied")
+                create()
+            }.show()
+            showPermissionDeniedDialog = false
+        }
+    }
+
+    private fun setUpMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just move the camera to Sydney and add a marker in Sydney.
+     */
+    override fun onMapReady(googleMap: GoogleMap?) {
+        map = googleMap ?: return
+
+        with(googleMap) {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(VALORI_LATITUDE_AND_LONGITUDE, ZOOM_LEVEL))
+            addMarker(MarkerOptions().position(VALORI_LATITUDE_AND_LONGITUDE))
+        }
+        map.uiSettings.isScrollGesturesEnabled = false
+        map.uiSettings.isZoomGesturesEnabled = true
+        enableMyLocation()
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(LOCATION_PERMISSION_REQUEST_CODE)
+    private fun enableMyLocation() {
+        // Enable the location layer. Request the location permission if needed.
+        val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (EasyPermissions.hasPermissions(context!!, *permissions)) {
+            map.isMyLocationEnabled = true
+        } else {
+            // if permissions are not currently granted, request permissions
+            EasyPermissions.requestPermissions(this,
+                    "permission rationale",
+                    LOCATION_PERMISSION_REQUEST_CODE, *permissions)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,
+                permissions, grantResults, this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+
 
 }
