02c4b6482794cd9f34838363fb65e596abd08f76
Rutger Korstjens <rutger.korstjens@gmail.com>
2018-12-21 14:36:05 +0100

    hide keyboard when clicking map with keyboard open

--- app/src/main/java/com/example/prestashopwebview/appInfo/AppInfoActivity.kt
+++ app/src/main/java/com/example/prestashopwebview/appInfo/AppInfoActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.prestashopwebview.R
+import com.example.prestashopwebview.Utils
 import com.example.prestashopwebview.data.AddressMap
 import kotlinx.android.synthetic.main.bottom_navigation_bar.*
 
@@ -38,6 +39,7 @@ class AppInfoActivity : AppCompatActivity(), ValoriMapFragment.OnMapSelected {
     }
 
     override fun onMapSelected() {
+        Utils.hideKeyboard(this)
         var mapFragment = supportFragmentManager.findFragmentByTag(ValoriMapFragment.FRAGMENT_TAG) as? ValoriMapFragment
         if (mapFragment == null) {
             mapFragment = ValoriMapFragment.newInstance(AddressMap(getString(R.string.app_info_body_text)))
