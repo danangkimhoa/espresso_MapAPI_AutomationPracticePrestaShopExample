5b00653496209c82e1f64eb3f4c609c0e12d9cf4
Rutger Korstjens <rutger.korstjens@gmail.com>
2019-01-16 12:58:56 +0100

    added hybrid test

--- app/src/androidTest/java/com/example/prestashopwebview/HybridTest.kt
+++ app/src/androidTest/java/com/example/prestashopwebview/HybridTest.kt
@@ -1,13 +1,29 @@
 package com.example.prestashopwebview
 
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.web.assertion.WebViewAssertions
+import androidx.test.espresso.web.model.Atoms
+import androidx.test.espresso.web.sugar.Web
+import androidx.test.espresso.web.webdriver.DriverAtoms
+import androidx.test.espresso.web.webdriver.DriverAtoms.*
+import androidx.test.espresso.web.webdriver.Locator
 import com.example.prestashopwebview.base.BaseTest
+import org.hamcrest.CoreMatchers
 import org.junit.Test
 
 class HybridTest: BaseTest() {
 
     @Test
     fun shouldBeAbleToReuseCodeFromWebAppTest(){
-
+        Web.onWebView()
+                .withElement(DriverAtoms.findElement(Locator.CLASS_NAME, "login"))
+                .perform(DriverAtoms.webClick())
+                .withElement(DriverAtoms.findElement(Locator.ID, "email_create"))
+                .perform(DriverAtoms.webKeys("Garbage"))
+                .withElement(findElement(Locator.NAME, "SubmitCreate"))
+                .perform(webClick())
+                .withElement(findElement(Locator.CSS_SELECTOR, "#center_column > div.alert.alert-danger > ol > li"))
+                .check(WebViewAssertions.webMatches(getText(), CoreMatchers.`is`(CoreMatchers.equalTo("Invalid email address."))))
     }
 
 }
